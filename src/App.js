3ec5f8644d77ee2a3782a62a82cc0ad4b3551fd6
Quiet Place <silencer.web@gmail.com>
2024-07-25 10:36:05 +0500

    adds pinning footer to the bottom of the screen even if there is not much of the content

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Header } from "./components/header";
 import { Footer } from "./components/footer";
 import { SearchResults } from "./components/search-results";
 
+import styles from "./App.module.scss";
+
 export function App() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
-      <SearchResults />
+      <main>
+        <SearchResults />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
