26a72393b7493db8550b7bfa7b4250ca66f22055
Quiet Place <silencer.web@gmail.com>
2024-07-25 10:47:01 +0500

    adds missing section in the component Footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,7 +9,7 @@ import { ReactComponent as Telegram } from "./images/telegram.svg";
 
 import styles from "./footer.module.scss";
 
-const menu = [
+const mainNavigation = [
   [
     { label: "О проекте", href: "/" },
     { label: "Блог", href: "/" },
@@ -35,48 +35,71 @@ export function Footer() {
   return (
     <footer className={styles.wrapper}>
       <div className={clsx("container", styles.inner)}>
-        <nav className={styles.navigation}>
-          {menu.map((items, index) => (
-            <ul className={styles["navigation-list"]} key={index}>
-              {items.map((item) => (
-                <li className={styles["navigation-item"]} key={item.label}>
-                  <a className={styles["navigation-link"]} href={item.href}>
-                    {item.label}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          ))}
-        </nav>
-        <ul className={styles.stores}>
-          <li className={styles["stores-item"]}>
-            <a className={styles["stores-link"]} href="/">
-              <AppStore />
-            </a>
-          </li>
-          <li className={styles["stores-item"]}>
-            <a className={styles["stores-link"]} href="/">
-              <GooglePlay />
-            </a>
-          </li>
-        </ul>
-        <ul className={styles.socials}>
-          <li className={styles["socials-item"]}>
-            <a className={styles["socials-link"]} href="/">
-              <Vkontakte />
-            </a>
-          </li>
-          <li className={styles["socials-item"]}>
-            <a className={styles["socials-link"]} href="/">
-              <Odnoklassniki />
-            </a>
-          </li>
-          <li className={styles["socials-item"]}>
-            <a className={styles["socials-link"]} href="/">
-              <Telegram />
-            </a>
-          </li>
-        </ul>
+        <div className={styles["main-section"]}>
+          <nav className={styles["main-navigation"]}>
+            {mainNavigation.map((items, index) => (
+              <ul className={styles["main-navigation-list"]} key={index}>
+                {items.map((item) => (
+                  <li
+                    className={styles["main-navigation-item"]}
+                    key={item.label}
+                  >
+                    <a
+                      className={styles["main-navigation-link"]}
+                      href={item.href}
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </nav>
+          <ul className={styles.stores}>
+            <li className={styles["stores-item"]}>
+              <a className={styles["stores-link"]} href="/">
+                <AppStore />
+              </a>
+            </li>
+            <li className={styles["stores-item"]}>
+              <a className={styles["stores-link"]} href="/">
+                <GooglePlay />
+              </a>
+            </li>
+          </ul>
+          <ul className={styles.socials}>
+            <li className={styles["socials-item"]}>
+              <a className={styles["socials-link"]} href="/">
+                <Vkontakte />
+              </a>
+            </li>
+            <li className={styles["socials-item"]}>
+              <a className={styles["socials-link"]} href="/">
+                <Odnoklassniki />
+              </a>
+            </li>
+            <li className={styles["socials-item"]}>
+              <a className={styles["socials-link"]} href="/">
+                <Telegram />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles["additional-section"]}>
+          <span className={styles.copyright}>© 2022 ООО «КОНТЕНТ»</span>
+          <ul className={styles.legal}>
+            <li className={styles["legal-item"]}>
+              <a className={styles["legal-link"]} href="/">
+                Политика конфиденциальности
+              </a>
+            </li>
+            <li className={styles["legal-item"]}>
+              <a className={styles["legal-link"]} href="/">
+                Пользовательское соглашение
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
