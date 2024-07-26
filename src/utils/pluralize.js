d70155e67dc796199e8e4a0d0cac9202413a6c6f
Quiet Place <silencer.web@gmail.com>
2024-07-26 09:40:13 +0500

    adds pluralize util

---
+++ src/utils/pluralize.js
@@ -0,0 +1,10 @@
+// Taken from https://gist.github.com/znechai/1b25d0ee9a92e5b879175ab4f040dbbc
+export function pluralize(count, words) {
+  const cases = [2, 0, 1, 1, 1, 2];
+  const word =
+    words[
+      count % 100 > 4 && count % 100 < 20 ? 2 : cases[Math.min(count % 10, 5)]
+    ];
+
+  return `${count} ${word}`;
+}
